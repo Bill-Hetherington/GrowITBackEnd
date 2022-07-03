300b62db99942e8c7f965dcf0b8b7cb7845d5342
billh <billh890@gmail.com>
2022-07-03 11:59:20 +0200

    added itemController

--- GrowITBackEnd/Controllers/ItemsController.cs
+++ GrowITBackEnd/Controllers/ItemsController.cs
@@ -1,6 +1,9 @@
-﻿using GrowITBackEnd.Data;
+﻿using ApiTemplate.Models.AuthModels.JWTAuthentication.NET6._0.Auth;
+using GrowITBackEnd.Data;
+using GrowITBackEnd.Models.DataModels;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace GrowITBackEnd.Controllers
 {
@@ -12,6 +15,102 @@ namespace GrowITBackEnd.Controllers
         public ItemsController(ApplicationDbContext context)
         {
             _context = context;            
+        }
+
+        //Triggered: Admin 
+        [HttpGet]
+        [Route("GetAllItems")]
+        public async Task<List<Item>> GetAllItems()
+        {
+            return _context.Items.ToListAsync().Result;
+        }
+
+        //Triggered: User clicks category name on the select category combo box
+        //Get items for that category
+        [HttpGet]
+        [Route("GetCategoryItems")]
+        public async Task<List<Item>> GetCategoryItems(string category)
+        {
+            return _context.Items.Where(i => i.Category == category).ToListAsync().Result;
+        }
+
+        //Triggered: Admin creates item
+        [HttpPost]
+        [Route("CreateItem")]
+        public async Task<IActionResult> CreateItem(Item item)
+        {
+            var newItem = new Item
+            {                
+                Item_Name=item.Item_Name,
+                Price=item.Price,
+                Description=item.Description,
+                Quantity_on_Hand=item.Quantity_on_Hand,
+                Category=item.Category
+            };
+            _context.Items.Add(newItem);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                return NotFound(new Response { Status = "Failed", Message = "Item Creation failed" });
+            }
+            return Ok(new Response { Status = "Success", Message = "Item Creation Successful" });
+        }
+
+        //Triggered by admin changing details of item
+        //Item id must not be editable
+        [HttpPut]
+        [Route("UpdateItem")]
+        public async Task<IActionResult> UpdateItem(int id,Item item)
+        {
+            if (id != item.ItemID)
+            {
+                return BadRequest(); 
+            }
+            _context.Entry(item).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ItemExists(id))
+                {
+                    return NotFound(new Response { Status = "Failed", Message = "Item not Found" });
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        //Triggered: Admin deletes item
+        [HttpDelete]
+        [Route("DeleteItem")]
+        public async Task<IActionResult> DeleteItem(Item item)
+        {
+            _context.Items.Remove(item);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                return NotFound(new Response { Status = "Failed", Message = "Item Deletion failed" });
+            }
+
+            return Ok(new Response { Status = "Success", Message = "Item Deleted Succesfully" });
+        }
+
+        private bool ItemExists(int id)
+        {
+            return (_context.Items?.Any(e => e.ItemID == id)).GetValueOrDefault();
         }
     }
 }
